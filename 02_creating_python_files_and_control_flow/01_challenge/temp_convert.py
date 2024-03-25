27220d877d29a5a93d43059633aacb3318989cf3
tomohiko.ozaki <tomohiko.ozaki@woven-planet.global>
2024-03-25 14:05:00 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,9 +1,27 @@
-def convert():
+
+def convert(temp, deg):
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    if (deg == "f"):
+        celsius = (temp - 32 ) * 5 / 9
+        return celsius
 
-    return temp
+    elif (deg == "c"):
+        fahrenheit = (temp * 9 / 5) + 32
+        return fahrenheit
 
 
-convert()
+def main():
+    temp = float(input("Enter the temperature: "))
+    deg = input("Enter the unit (c for Celsius, f for Fahrenheit): ")
+
+    converted_temp = convert(temp, deg)
+
+    if isinstance(converted_temp, float):
+        if deg == "f":
+            print(f"{temp} degrees Fahrenheit is {converted_temp} degrees Celsius.")
+        elif deg == "c":
+            print(f"{temp} degrees Celsius is {converted_temp} degrees Fahrenheit.")
+    else:
+        print(converted_temp)
+main()
