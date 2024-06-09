872351dc6990d1d8d7deb749ff0226ac9c95313a
tomohiko.ozaki <tomohiko.ozaki@woven-planet.global>
2024-06-09 10:52:51 +0900

    Submit challenge

--- 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
+++ 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
@@ -1,1 +1,15 @@
 # ここにコードを書いてください
+for x in range(1,101):
+    if (x % 3 ==0 and x % 5 == 0):#3と5の公倍数の場合は `FizzBuzz`
+        print("FizzBuzz")
+    
+    elif (x % 5 == 0):#5の倍数の場合は `Buzz`
+        print("Buzz")
+        
+    elif (x % 3 ==0):#3の倍数の場合は数字の代わりに `Fizz`
+        print("Fizz")
+    
+    else:
+        print(x)
+    
+     
