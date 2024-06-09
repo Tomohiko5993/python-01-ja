1423f32aefa2f69e14bfd1d12ed39115c70107f3
tomohiko.ozaki <tomohiko.ozaki@woven-planet.global>
2024-06-09 10:07:22 +0900

    Submit challenge

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -7,7 +7,7 @@ vowel_counter = 0 #母音のカウンターを初期化する
 for char in count_words:# hello world ->h,e,l,l,o ...
     if char in vowels:#aeiouのリストに合致するかを確認し、Trueなら以下の条件に従う
         vowel_counter = vowel_counter + 1#aeiouに合致したらカウントを増やす
-print(f"Number of vowels: {vowel_counter} ")
+print(f"Number of vowels:{vowel_counter} ")
 
 
 
