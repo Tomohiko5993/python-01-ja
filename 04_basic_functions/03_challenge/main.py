49b335322a2dc906c29c6073a0336db6c2d9bf99
tomohiko.ozaki@woven.toyota <tomohiko.ozaki@woven-planet.global>
2024-08-17 14:47:48 +0900

    Submit challenge

--- 04_basic_functions/03_challenge/main.py
+++ 04_basic_functions/03_challenge/main.py
@@ -1,7 +1,18 @@
 def unique_substrings(input_string):
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    #pass
+    #部分もじを格納するためのセットを使用
+    substrings = set()
 
+    #すべての開始位置を考慮
+    for start in range(len(input_string)):
+        #すべての終了位置を考慮
+        for end in range(start + 1, len(input_string) + 1):
+            #部分文字をセットに追加
+            substrings.add(input_string[start:end])
+
+    #セットをリストに変換して返す
+    return sorted(substrings)
 
 input_string = "banana"
 print(unique_substrings(input_string))
